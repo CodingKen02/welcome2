00ebc4b036d843494f185449d42460a3254804f3
CodingKen02 <71536021+CodingKen02@users.noreply.github.com>
2023-01-18 23:29:27 -0600

    Initial commit

---
+++ Welcome2.java
@@ -0,0 +1,28 @@
+/*
+Name: Kennedy Keyes
+Date: February 11, 2021
+Name of Program: Welcome2.java
+Description: This program will print a line of text with multiple statements. 
+ */
+package welcome2;
+
+/**
+ *
+ * @author codingken
+ */
+public class Welcome2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    // main method begins execution of Java application
+    public static void main(String[] args) 
+    {
+        // print line of text using multiple statements
+        System.out.print("Welcome to ");
+        System.out.println("Java Programming!");
+        
+    } // end method main
+    
+} // end class Welcome2
